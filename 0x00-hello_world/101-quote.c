981f99994e7610d80d26c94b92f942b65cd2362e
Devjabli <aroussiotmane27@gmail.com>
2023-09-08 23:08:43 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point 
  *
@@ -8,8 +7,6 @@
  *
  * Return: 1 (error).
 */
-
-
 int main(void)
 {       
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
