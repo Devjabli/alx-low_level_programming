91db71352a2c21499dbeee8b377ee93e59f8ba7c
Devjabli <aroussiotmane27@gmail.com>
2023-09-08 09:04:17 +0100

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * Main -> entry point
+ * main -> Entry point
  *
  * Description : this program will print a string with function put
  *
- * Return: 0 (success)
+ * Return: Always  (0) success
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
