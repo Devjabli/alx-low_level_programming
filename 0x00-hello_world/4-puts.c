cc2c10fc78b09f5a778a115ef086ba77918f371a
Devjabli <aroussiotmane27@gmail.com>
2023-09-08 01:47:56 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Main -> entry point
+ * Description : this program will print a string with function put
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
