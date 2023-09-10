9b3d85dcae17868e344e77509a440c7aad547efd
otmane <aroussiotmane27@gmail.com>
2023-09-10 11:27:45 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all the numbers of base 16
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char value[] = "0123456789123456";
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(value[i]);
+	}
+	putchar('\n');
+	return (0);
+}
