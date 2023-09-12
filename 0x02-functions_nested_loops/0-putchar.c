a7ab5d50c41e3c24ca7b0d0ec006d3a3debda336
otmane <aroussiotmane27@gmail.com>
2023-09-12 22:43:05 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+        char st[8] = "_putchar";
+
+        printf("%s\n", st);
+        return (0);
+}
