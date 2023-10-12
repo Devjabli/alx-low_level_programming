2c80f3a4f971c94e7b51b5ca59fe5e89fb91a09a
otmane <aroussiotmane27@gmail.com>
2023-10-13 00:42:38 +0100

    2-print_strings.c

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -4,10 +4,9 @@
  * main - check the code
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
-	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_strings(", ", 2, "Jay", "Django");
 	return (0);
 }
