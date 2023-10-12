f897c56f8fa0defa73ba7510f26d8546aa475348
otmane <aroussiotmane27@gmail.com>
2023-10-13 00:30:33 +0100

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (i != (n - 1))
-			printf("%s", separator);
-		else if (separator != NULL)
+		if (i != n - 1 && separator != NULL) 
 			printf("%s", separator);
 	}
 
