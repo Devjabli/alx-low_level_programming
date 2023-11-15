f3e835cf4ebab95874353dc4c09542ba24501406
Devjabli <aroussiotmane27@gmail.com>
2023-11-16 00:20:49 +0100

    recursion_1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,18 @@
   *
   * Return: Always 0.
   */
+
+
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
 	{
-	_putchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
 
