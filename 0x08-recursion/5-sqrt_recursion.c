3beb413865c14f46c28c9a4966776b15ce20a779
Devjabli <aroussiotmane27@gmail.com>
2023-11-16 23:08:32 +0100

    sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+int rec_sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - main function
+ *
+ * @n: parameter function
+ *
+ * Return - n
+*/
+
+
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (rec_sqrt_recursion(n, 0));
+}
+
+/**
+ * rec_sqrt_recursion - recurses the result
+ *
+ * @n: value of squar
+ * @i: iterator
+ *
+ * Return: resutling square
+*/
+
+int rec_sqrt_recursion(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (rec_sqrt_recursion(n, i+1));
+
+}
