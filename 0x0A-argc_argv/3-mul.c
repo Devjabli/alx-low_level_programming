cd7d181b200b1b8b4c2d6891daa0112f341d0f1e
Devjabli <aroussiotmane27@gmail.com>
2023-11-18 22:12:37 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-/*
- * main - Entry point.
- * _atoi - converts a string to an integer
- * @argc: number of command line.
- * @argv: andicate to an array of command line arguments.
- * Return: 0
-*/
+/**
+ * main - The main function for the multiplication program.
+ * @argc: The number of command-line arguments.
+ * @argv: The array of command-line argument strings.
+ *
+ * Return: 0 if the program executed successfully, 1 otherwise.
+ */
 
 int main(int argc, char *argv[])
 {
 	int s;
 
-	if (argc < 4)
+	if (argc == 4)
 	{
 		s = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", s);
