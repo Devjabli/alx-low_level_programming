d97e26aaebb5ff55f6c6edaded156033d7a32fec
otmane <aroussiotmane27@gmail.com>
2023-11-29 17:56:28 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - takes char name and function pointer f as a paramters
+ * @f: function pointer
+ * @name: char pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
