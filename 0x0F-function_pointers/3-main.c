19701a2f639e559b7fb3dce49ee76692e6b8908b
otmane <aroussiotmane27@gmail.com>
2023-11-30 14:09:47 +0100

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,11 +9,11 @@
  * @argv: An array of pointers to the arguments.
  * Return: Always 0.
 */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -36,7 +36,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
