b50288b16b29156a318db5d81ea4cad3192c1d95
otmane <aroussiotmane27@gmail.com>
2023-12-11 16:15:50 +0100

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,18 +4,18 @@
 /**
  * print_listint - printing link list
  * @h: pointing to the first node
- * Return: total of itration (total)
+ * Return: total of itration (i)
 */
 
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h);
 {
 	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d \n", h->n);
-		h = h->next;
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,15 +12,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (!head || !newNode)
-	{
 		return (NULL);
-	}
-	else
-	{
-		newNode->next = NULL;
-		newNode->n = n;
-		newNode->next = *head;
-		*head = newNode;
-	}
+	newNode->next = NULL;
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+
 	return (newNode);
 }
