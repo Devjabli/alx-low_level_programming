815078f9b61d0b0652842626313da76067be8842
Devjabli <aroussiotmane27@gmail.com>
2023-12-12 20:51:13 +0100

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
 /**
- * free_listint - frees lists
+ * free_listint2 - frees lists
  * @head: pointer to the first node
  * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-    listint_t *new, *tmp;
+	listint_t *new, *tmp;
 
-    if (!head)
-        return;
+	if (!head)
+		return;
 
-    new = *head;
+	new = *head;
 
-    while (new)
-    {
-        tmp = new;
-        new = new->next;
-        free(tmp);
-    }
-    *head = NULL;
+	while (new)
+	{
+		tmp = new;
+		new = new->next;
+		free(tmp);
+	}
+	*head = NULL;
 }
