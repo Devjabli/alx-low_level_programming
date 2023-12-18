ddc93a32cd2ec2fb2d73a59502bd0ce89ae1083f
otmane <aroussiotmane27@gmail.com>
2023-12-18 23:15:27 +0100

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * strlen - returns the length
+ * @c: string lead for length to check
+ *
+ * Return: int length
+*/
+
+int strlen(char *c)
+{
+	int i = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+
+
+/**
+ * append_text_to_file - appending text to file
+ * @filename: pointer filename
+ * @text_content: txt to write
+ *
+ * Return: if succes 1 otherwise if fails 0
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fl;
+	ssize_t sizes = 0;
+	ssize_t len = strlen(text_context);
+
+	if (!filename)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_APPEND);
+	if (fl == -1)
+		return (-1);
+	if (len)
+		sizes = write(fl, text_context, len);
+	close(fl);
+	return (sizes == len ? 1 : -1);
+}
