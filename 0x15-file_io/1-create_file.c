e4baafda68d461ba4533c3de47d0c538199a97a7
Devjabli <aroussiotmane27@gmail.com>
2023-12-21 16:55:18 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+
+/**
+ * _strlen - returns the length
+ * @c: string lead for length to check
+ *
+ * Return: int length
+*/
+
+int _strlen(char *c)
+{
+	int i = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+
+
 /**
  * create_file - function that creating  a file
  * @filename: pointer filename
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length
+ * @c: string lead for length to check
+ *
+ * Return: int length
+*/
+
+int _strlen(char *c)
+{
+	int i = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+
+
+/**
  * append_text_to_file - appending text to file
  * @filename: pointer filename
  * @text_content: txt to write
